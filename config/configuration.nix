6e91341befb54e5592b6bb00386464357399d7d7
Tan Quach <t@nqua.ch>
2022-12-04 06:54:33 +0000

    enable natural scrolling in libinput

--- config/configuration.nix
+++ config/configuration.nix
@@ -100,6 +100,8 @@
 
   # Enable touchpad support (enabled default in most desktopManager).
   services.xserver.libinput.enable = true;
+  services.xserver.libinput.touchpad.naturalScrolling = true;
+  services.xserver.libinput.mouse.naturalScrolling = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.tanq = {
