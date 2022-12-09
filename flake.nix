6dc9c7624e81557513428dbce2f8193435bf7f46
Tan Quach <t@nqua.ch>
2022-12-09 17:57:59 +0000

    changed channels from nixos-unstable to nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A NixOS installation flake for my Framework laptop";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
