521e320a8e36d9871f3aff9103133ac9fc366f0c
Tan Quach <t@nqua.ch>
2022-12-11 09:38:07 +0000

    fix vim colours in alacritty terminal

--- config/user/programs/vim.nix
+++ config/user/programs/vim.nix
@@ -100,7 +100,12 @@ let g:session_command_aliases = 1
 "*****************************************************************************
 "" Visual Settings
 "*****************************************************************************
-syntax on
+
+let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
+let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
+set termguicolors
+
+syntax enable
 set ruler
 silent! colorscheme night-owl
 
